5fe3b25cd5e1c6b47eead55b961ae175648a3565
Sonal-BP <108402618+Sonal-BP@users.noreply.github.com>
2024-09-08 12:56:02 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,19 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
+output "pri_sub_nw_addr_1" {
+  description = "The CIDR block of the first private subnet"
+  value       = aws_subnet.private_subnet_1.cidr_block
 }
 
-output "public_subnet_ids" {
-  value = [
-    aws_subnet.public_subnet_1.id,
-    aws_subnet.public_subnet_2.id
-  ]
+output "pri_sub_nw_addr_2" {
+  description = "The CIDR block of the second private subnet"
+  value       = aws_subnet.private_subnet_2.cidr_block
 }
 
-output "private_subnet_ids" {
-  value = [
-    aws_subnet.private_subnet_1.id,
-    aws_subnet.private_subnet_2.id
-  ]
+output "pub_sub_nw_addr_1" {
+  description = "The CIDR block of the first public subnet"
+  value       = aws_subnet.public_subnet_1.cidr_block
 }
 
-output "security_group_id" {
-  value = aws_security_group.allow_all.id
+output "pub_sub_nw_addr_2" {
+  description = "The CIDR block of the second public subnet"
+  value       = aws_subnet.public_subnet_2.cidr_block
 }
