fc7921b36e7921121cc9ff8ce0ff031bd94d8263
Sonal-BP <108402618+Sonal-BP@users.noreply.github.com>
2024-09-08 12:54:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 provider "aws" {
-  region = var.aws_region
+  region     = "us-west-2"
+ 
 }
 
+# VPC
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -11,10 +13,11 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Public Subnets
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet_cidrs[0]
-  availability_zone       = var.public_subnet_azs[0]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidrs[0]
+  availability_zone = "us-west-2a"
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet-1"
@@ -22,19 +25,20 @@ resource "aws_subnet" "public_subnet_1" {
 }
 
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet_cidrs[1]
-  availability_zone       = var.public_subnet_azs[1]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidrs[1]
+  availability_zone = "us-west-2b"
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet-2"
   }
 }
 
+# Private Subnets
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_cidrs[0]
-  availability_zone = var.private_subnet_azs[0]
+  availability_zone = "us-west-2c"
   tags = {
     Name = "private-subnet-1"
   }
@@ -43,12 +47,13 @@ resource "aws_subnet" "private_subnet_1" {
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_cidrs[1]
-  availability_zone = var.private_subnet_azs[1]
+  availability_zone = "us-west-2d"
   tags = {
     Name = "private-subnet-2"
   }
 }
 
+# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = {
@@ -56,6 +61,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+# Public Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   route {
@@ -67,6 +73,7 @@ resource "aws_route_table" "public" {
   }
 }
 
+# Public Route Table Association for Public Subnets
 resource "aws_route_table_association" "public_subnet_1" {
   subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public.id
@@ -77,19 +84,36 @@ resource "aws_route_table_association" "public_subnet_2" {
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table" "private" {
+# Security Groups
+resource "aws_security_group" "allow_all" {
   vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "private-route-table"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-}
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_all"
+  }
 
-resource "aws_route_table_association" "private_subnet_1" {
-  subnet_id      = aws_subnet.private_subnet_1.id
-  route_table_id = aws_route_table.private.id
-}
-
-resource "aws_route_table_association" "private_subnet_2" {
-  subnet_id      = aws_subnet.private_subnet_2.id
-  route_table_id = aws_route_table.private.id
+  #Another inbound & outbound rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
