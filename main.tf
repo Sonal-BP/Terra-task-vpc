1545601ccfdbbfd5f3b62ec955f6ff6b8ae54cf6
Sonal-BP <108402618+Sonal-BP@users.noreply.github.com>
2024-09-10 16:51:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -78,6 +78,23 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_security_group" "alb_sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
 
