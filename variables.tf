c12b43b4be3d59de8d7ea4f49af10d75b953fc09
Sonal-BP <108402618+Sonal-BP@users.noreply.github.com>
2024-09-10 13:31:56 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,11 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources"
-  default     = "us-west-2"
-}
-
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  default     = "10.0.0.0/16"
+  type = string
 }
 
 variable "public_subnet_cidrs" {
-  description = "List of CIDR blocks for the public subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  type = list(string)
 }
 
 variable "private_subnet_cidrs" {
-  description = "List of CIDR blocks for the private subnets"
-  type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+  type = list(string)
 }
