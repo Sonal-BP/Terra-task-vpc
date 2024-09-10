ddbb61d23efdc6ca99bd56486153b5ce5fad3982
Sonal-BP <108402618+Sonal-BP@users.noreply.github.com>
2024-09-10 13:31:28 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,36 +1,16 @@
 output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.main.id
-}
-
-output "private_subnet_ids" {
-  description = "List of private subnet IDs"
-  value       = [
-    aws_subnet.private_subnet_1.id,
-    aws_subnet.private_subnet_2.id
-  ]
+  value = aws_vpc.main.id
 }
 
 output "public_subnet_ids" {
-  description = "List of public subnet IDs"
-  value       = [
-    aws_subnet.public_subnet_1.id,
-    aws_subnet.public_subnet_2.id
-  ]
+  value = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
 }
 
-output "private_subnet_cidrs" {
-  description = "List of CIDR blocks for private subnets"
-  value       = [
-    aws_subnet.private_subnet_1.cidr_block.id,
-    aws_subnet.private_subnet_2.cidr_block.id
-  ]
+output "private_subnet_ids" {
+  value = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
 }
 
-output "public_subnet_cidrs" {
-  description = "List of CIDR blocks for public subnets"
-  value       = [
-    aws_subnet.public_subnet_1.cidr_block.id,
-    aws_subnet.public_subnet_2.cidr_block.id
-  ]
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.main.id
 }
